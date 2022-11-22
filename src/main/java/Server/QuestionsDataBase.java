f8be8f3c10586ce3e3e85ecb18f9a79b80f3e8ad
lcj <lcj@kth.se>
2022-11-22 23:30:08 +0100

    Jag har skapat en ny klass som heter QuestionsDataBase som läser in en textfil där alla frågor står (tillsammans med rätt svar, kategori osv) Som sen skapar Question-objekt av alla dessa frågor och sedan läggs det till i en Hashmap. Sedan i QuestionsDataBase har jag skapat metoder så att man kan hämta frågor från given kategori på ett slumpmässigt sätt. Har även lagt till andra metoder som: * getQuestionsFromCategory() - returnerar alla frågor i en array för den kategorin * getRandomQuestionFromCategory() - returnerar slumpmässig fråga givet en kategori * getNumberOfQuestionsFromCategory() - returnerar antalet frågor som finns i en kategori * getNumberOfQuestions() - returnerar totala antalet frågor över alla kategorier

---
+++ src/main/java/Server/QuestionsDataBase.java
@@ -0,0 +1,128 @@
+package Server;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class QuestionsDataBase
+{
+    HashMap<String, ArrayList<Question>> db = new HashMap<>();
+    int numberOfQuestions;
+    QuestionsDataBase(Path p)
+    {
+        this.db = loadDataBase(p);
+        this.numberOfQuestions = 0;
+    }
+
+    private HashMap<String, ArrayList<Question>> loadDataBase(Path p)
+    {
+        String tempLine;
+        String questionDescription;
+        String[] alternatives = new String[4];
+        String categoryString;
+        int correctAnswerIndex = 0;
+        try (FileReader fr = new FileReader(String.valueOf(p));
+             BufferedReader br = new BufferedReader(fr))
+        {
+            while ((tempLine = br.readLine()) != null)
+            {
+                // Hämtar den första raden som är självaste frågan
+                questionDescription = tempLine.strip();
+                // går till nästa rad och hämtar svarsalternativen
+                if ((tempLine = br.readLine()) != null)
+                {
+                    alternatives = tempLine.split(",");
+                }
+                // går till nästa rad och hämtar index till rätt svar
+                if ((tempLine = br.readLine()) != null)
+                {
+                    correctAnswerIndex = Integer.parseInt(tempLine);
+
+                }
+                // går till nästa rad och hämtar kategorin till frågan
+                if ((tempLine = br.readLine()) != null)
+                {
+                    categoryString = tempLine.strip();
+                }
+                else
+                {
+                    break;
+                }
+                // skapa frågan
+                Question q = new Question(questionDescription, alternatives, correctAnswerIndex);
+                // lägg till frågan i databasen
+                addQuestionToDatabase(q, categoryString);
+                numberOfQuestions++;
+            }
+
+        } catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+        return db;
+    }
+    public void addQuestionToDatabase(Question question, String category){
+        ArrayList<Question> arrayOfQuestions;
+        if(!db.containsKey(category)){
+            arrayOfQuestions = new ArrayList<>();
+            arrayOfQuestions.add(question);
+            db.put(category, arrayOfQuestions);
+        }
+        else{
+            arrayOfQuestions = db.get(category);
+            arrayOfQuestions.add(question);
+        }
+    }
+
+    public ArrayList<Question> getQuestionsFromCategory(String category){
+        return db.get(category);
+    }
+
+    public Question getRandomQuestionFromCategory(String category){
+        Random rand = new Random();
+        int randomIndex = rand.nextInt(getNumberOfQuestionsFromCategory(category));
+        return getQuestionsFromCategory(category).get(randomIndex);
+    }
+
+    public int getNumberOfQuestionsFromCategory(String category){
+        return db.get(category).size();
+    }
+
+    public int getNumberOfQuestions(){
+        return this.numberOfQuestions;
+    }
+
+    public void showAllCategories(){
+        System.out.println("Categories: \n" );
+        for(String s : db.keySet()){
+            System.out.println(s);
+        }
+    }
+
+    public void showDatabase(){
+        for(String s : db.keySet()){
+            System.out.println("Category " + s + " has questions: ");
+            System.out.println(db.get(s));
+        }
+    }
+
+    public static void main(String[] args) throws FileNotFoundException
+    {
+        Path p = Paths.get("src/main/java/Server/questions.txt");
+        QuestionsDataBase d = new QuestionsDataBase(p);
+        Question q = d.getRandomQuestionFromCategory("Sport");
+        System.out.println(q.description);
+        System.out.println();
+    }
+
+}
+
+
+
+/*
+
+ */
