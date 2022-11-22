ec85b145689301b4c26fbf224ee9406fc09b3598
Frida Lindberg <fridalindberg03@gmail.com>
2022-11-22 18:29:49 +0100

    fix av problem

--- src/main/java/com/example/quizkampen/HelloApplication.java
+++ src/main/java/com/example/quizkampen/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application
     @Override
     public void start(Stage stage) throws IOException
     {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("scoreScene.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("quizScene.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 320, 500);
         stage.setTitle("Quizkampen");
         stage.setScene(scene);
